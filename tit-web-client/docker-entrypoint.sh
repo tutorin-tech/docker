456b5f51567d0020c4944d0e04859411c8efaec9
Denis Gavrilyuk <karpa4o4@gmail.com>
2021-07-06 16:32:33 +0300

    tit-web-client: pass REACT_APP_TIT_THEME_HOST

--- tit-web-client/docker-entrypoint.sh
+++ tit-web-client/docker-entrypoint.sh
@@ -6,6 +6,8 @@ export PORT="${PORT:="8001"}"
 
 export REACT_APP_TIT_RPC_SERVER_URL="${REACT_APP_TIT_RPC_SERVER_URL}"
 
+export REACT_APP_TIT_THEME_HOST="${REACT_APP_TIT_THEME_HOST}"
+
 set +x
 
 sed -i -e "s/PORT/${PORT}/" /etc/nginx/conf.d/default.conf
