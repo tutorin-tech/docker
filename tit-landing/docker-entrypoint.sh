c77d2ccb95103e65c71c4a9eaf07824e07c1301d
Denis Gavrilyuk <karpa4o4@gmail.com>
2021-07-21 14:05:05 +0300

    tit-landing: replace '%METRIKA_COUNTER%' in template

--- tit-landing/docker-entrypoint.sh
+++ tit-landing/docker-entrypoint.sh
@@ -4,6 +4,11 @@ set -x
 
 PORT="${PORT:="8000"}"
 
+METRIKA_COUNTER="${METRIKA_COUNTER}"
+
+# Escapes regex characters.
+METRIKA_COUNTER="$(echo "${METRIKA_COUNTER}" | sed -e 's/[\/&]/\\&/g')"
+
 export ALLOWED_HOSTS="${ALLOWED_HOSTS:="127.0.0.1"}"
 
 export DEBUG="${DEBUG}"
@@ -17,5 +22,7 @@ export TIT_API_HOST="${TIT_API_HOST:="http://127.0.0.1:8003"}"
 set +x
 
 sed -i -e "s/PORT/${PORT}/" /var/www/tit-landing/uwsgi.ini
+
+sed -i -e "s/%METRIKA_COUNTER%/${METRIKA_COUNTER}/" /var/www/tit-landing/templates/layouts/base.html
 
 uwsgi --ini /var/www/tit-landing/uwsgi.ini
